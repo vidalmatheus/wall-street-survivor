376d267e98c6eba33a91159e4abdd2626e4f3b59
Matheus Vidal <matheusvidaldemenezes@gmail.com>
2023-10-04 02:31:19 -0300

    Validate datetime format as asked

--- datacon/schemas.py
+++ datacon/schemas.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from ninja import Schema
+from pydantic import validator
 
 
 class FetchTransactionsSchema(Schema):
@@ -6,3 +9,22 @@ class FetchTransactionsSchema(Schema):
     password: str
     start_date: str
     end_date: str
+
+    @validator("start_date")
+    def convert_date_format_start(cls, value):
+        try:
+            input_date = datetime.strptime(value, "%Y-%m-%d")
+            formatted_date = input_date.strftime("%m-%d-%Y")
+            return formatted_date
+        except ValueError:
+            raise ValueError("Invalid date format. Use 'YYYY-MM-DD'.")
+
+    @validator("end_date")
+    def convert_date_format_end(cls, value):
+        try:
+            input_date = datetime.strptime(value, "%Y-%m-%d")
+            formatted_date = input_date.strftime("%m-%d-%Y")
+
+            return formatted_date
+        except ValueError:
+            raise ValueError("Invalid date format. Use 'YYYY-MM-DD'.")
